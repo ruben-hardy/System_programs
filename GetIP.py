9c57696f9be5d30b6b8e93a249f409da44aee114
Ruban Kumar <ruban.kumar@hpe.com>
2018-03-16 11:42:22 +0530

    changed input to raw_input

--- GetIP.py
+++ GetIP.py
@@ -3,13 +3,13 @@ import subprocess
 
 def _test(cmd="ipconfig"):
     while True:
-        choice = input("Enter your choice\nPress 1 for os.system\nPress 2 for subprocess\nPress 3 to quit\n")
+        choice = raw_input("Enter your choice to check system IP\nPress 1 for os.system\nPress 2 for subprocess\nPress 3 to quit\n")
         try:
-            if choice == 1:
+            if int(choice) == 1:
                 os.system(cmd)
                 print("Exectuted using os.sytem")
                 return 0
-            elif choice == 2:
+            elif int(choice) == 2:
                 e = subprocess.Popen(cmd, stdout=subprocess.PIPE)
                 handle = e.stdout.readlines()
                 for line in handle:
@@ -18,8 +18,8 @@ def _test(cmd="ipconfig"):
                         print "Your system IP is:", l[1]
                 print "Executed using Subprocess.Popen"
                 return 0
-            elif choice == 3:
-                break
+            elif int(choice) == 3:
+                continue
         except NameError, ValueError:
             print "Input only numbers"
 
